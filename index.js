fe712d9e2a6c32ff9896babdcf0623711d22dce8
Maclemon17 <maclemon17@gmail.com>
2022-10-28 14:48:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(
+        {
+            "slackUsername": "Maclemon",
+            "backend": true,
+            "age": 23,
+            "bio": "I am Elijah Tosin Adegoke, a student and software deveoper from Oyo state, Nigeria. Born december 17"
+        }
+    )
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server Running on PORT: ${PORT}`)
+})
